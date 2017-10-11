88bf09c6b3aa67465ecbc8ed9bd8b42643e5f6b8
leijian <13528794119@163.com>
2017-10-11 11:21:50 +0800

    ngrx增加日志

--- src/app/ngrx/index.ts
+++ src/app/ngrx/index.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
-import { StoreModule, combineReducers, ActionReducer, compose } from '@ngrx/store';
+import { StoreModule, combineReducers, ActionReducer, compose, ActionReducerMap, MetaReducer } from '@ngrx/store';
 import { StoreRouterConnectingModule, routerReducer } from '@ngrx/router-store';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+
 import { storeFreeze } from 'ngrx-store-freeze';
 
 import * as fromTest from './reducers/test.reducer';
+import { environment } from '../../environments/environment';
 
 export interface State {
   test: fromTest.State
@@ -14,21 +16,28 @@ const initialState: State = {
   test: fromTest.initialState
 };
 
-const reducers = {
+const reducers: ActionReducerMap<State> = {
   test: fromTest.reducer
 }
 
-const productionReducers: ActionReducer<State> = combineReducers(reducers)
-// const developmentReducers: ActionReducer<State> = compose(storeFreeze, combineReducers)(reducers);
+// console.log all actions
+function logger(reducer: ActionReducer<State>): ActionReducer<State> {
+  return function(state: State, action: any): State {
+    console.log('state', state);
+    console.log('action', action);
 
-export function reducer(state = initialState, action: {type: string, payload: any} ): State {
-  return productionReducers(state, action)
+    return reducer(state, action);
+  };
 }
+
+const metaReducers: MetaReducer<State>[] = !environment.production
+? [logger]
+: [];
 
 @NgModule({
   imports: [
     // StoreModule.forRoot({ routerReducer: routerReducer }),
-    StoreModule.forRoot(reducers),
+    StoreModule.forRoot(reducers, { metaReducers }),
     StoreRouterConnectingModule,
     StoreDevtoolsModule.instrument({
       maxAge: 25 //  Retains last 25 states
